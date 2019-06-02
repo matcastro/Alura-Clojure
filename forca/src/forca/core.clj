f0b986f40cfd9ddeb8516aab5f015dbe0b8746f6
Mateus Castro <matcastro>
2019-06-02 13:46:56 -0300

    Listas, conjuntos e passando funções para funções.

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -4,13 +4,22 @@
 (def total-de-vidas 6)
 
 (defn perdeu [] (print "Voce perdeu!"))
+(defn ganhou [] (print "Voce ganhou!"))
 
-(defn jogo [vidas] 
+(defn letras-faltantes [palavra acertos]
+	(remove (fn [letra] (contains? acertos (str letra))) palavra)
+)
+
+(defn acertou-toda-a-palavra? [palavra acertos] 
+	(empty? (letras-faltantes palavra acertos))
+)
+
+(defn jogo [vidas palavra acertos] 
 	(if (= vidas 0)
 		(perdeu)
-		(do 
-				(print vidas)
-				(jogo (dec vidas))
+		(if (acertou-toda-a-palavra? palavra acertos) 
+			(ganhou) 
+			(print "Chuta a próxima letra.")
 		)
 	)
 )
@@ -19,6 +28,13 @@
 	(if (= n 0) 0
 		(if (= n 1) 1
 		 (+ (fib (dec n)) (fib (- n 2))))))
+
+(defn filtro [nums]
+	(filter 
+		(fn [x] (or (> x 4) (< x 2))
+		) nums
+	)
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
